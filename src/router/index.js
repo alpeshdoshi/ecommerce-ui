524bdc963040c602555fdc22605a84f6ac76bd3e
Alpesh Doshi <alpesh.doshi@bestbuy.com>
2022-06-24 12:26:31 -0500

    Stripe integration

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,10 @@ import Signup from "../views/Signup";
 import Signin from "../views/Signin";
 import WishList from "../views/Product/WishList"
 import Cart from "../views/cart/Cart"
+import Success from "../views/payment/Success";
+import Failed from "../views/payment/Failed"
+import Checkout from "../views/checkout/Checkout"
+
 const routes = [
   {
     path: "/",
@@ -72,6 +76,21 @@ const routes = [
     name : 'Cart',
     component : Cart
   },
+  {
+    path: '/payment/success',
+    name: 'PaymentSuccess',
+    component:Success
+  },
+  {
+    path: '/payment/failed',
+    name: 'FailedPayment',
+    component:Failed
+  },
+  {
+    path : '/checkout',
+    name : 'Checkout',
+    component : Checkout
+  }
 ];
 
 const router = createRouter({
